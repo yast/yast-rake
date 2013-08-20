2cd1aa9ff9be77d100ba3c70cd9a73c17d685462
Vladimir Moravec <vmoravec@suse.com>
2013-08-20 15:04:56 +0200

    Replace `sh` command for `system` in command definition to not depend on rake

--- rake/commands/gem.rb
+++ rake/commands/gem.rb
@@ -5,15 +5,16 @@ module Yast::Rake::Command
     def install
       build
       puts "Installing #{rake.config.gem.name} from #{rake.config.gem.path} ..."
-      sh "gem install #{rake.config.gem.path}"
+      system "gem install #{rake.config.gem.path}"
     end
 
     def build
       package = rake.config.package
       gem     = rake.config.gem
 
-      sh "gem build #{gem.spec}"
-      puts "Gem file is available in #{package.dir.join gem.name}"
+      system "gem build #{gem.spec}"
+      puts "Copying the gem file to package/ directory..."
+      cp gem.name, package.dir.join(gem.name)
       rm gem.name
     end
 
